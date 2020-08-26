54221e7b38a95b0b667db13343f72a9a6bf3768c
Manuela Scotto <manuelascotto18@gmail.com>
2020-08-26 16:21:41 +0200

    spiegazione

---
+++ README.md
@@ -0,0 +1,13 @@
+Creazione di un form 
+- Validazione per lo username, email e password
+- Spostamento in un'altra pagina se tutti i dati richiesti sono validi
+- Creazione di classi css per una migliore UI (se sbaglio mi indica dove e perchè)
+- Pulizia dei campi e delle evenutuali classi aggiunte
+
+utilizzo di:
+location.href 
+espressioni regolari
+classList.remove('')
+classList.add(')
+classList.contains('')
+document.form.submit()
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// COSTANTI
+///////////////////////////////////////////////////
+/////                 COSTANTI      
+/////////////////////////////////////////////////
+
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
@@ -11,22 +14,35 @@ const smallUser = document.querySelector('.user > small');
 const smallEmail = document.querySelector('.email > small');
 const smallPass = document.querySelector('.pass > small');
 const smallPass2 = document.querySelector('.pass2 > small');
-const btn = document.querySelector('.btn');
+const newAccount = document.getElementById('newAccount')
 
-// EVENTO
-form.addEventListener('submit', addAccount);
+/////////////////////////////////////////////////
+/////                 EVENTO      
+/////////////////////////////////////////////////
 
-// CALLBACK
+/* creo la condizione location per controllare il path: se sono in index.html allora faccio l'evento altrimenti se mi trovo in account.html stampa il testo */
+if (location.href === 'file:///C:/Users/scman/OneDrive/Desktop/esercitazioni/js/form/index.html') {
+    form.addEventListener('submit', addAccount);
+} else if (location.href === 'file:///C:/Users/scman/OneDrive/Desktop/esercitazioni/js/form/account.html?') {
+    newAccount.innerHTML = 'REGISTRAZIONE AVVENUTA CON SUCCESSO!!'
+}
+///////////////////////////////////////////////////
+/////                 CALLBACK
+/////////////////////////////////////////////////
+
 function addAccount(e) {
+    // espressioni regolari
     let userValid = /^[a-z0-9]{4,25}$/i;
     let emailValid = /\w+@\w+\.\w{2,4}/i;
     let passwordValid = /^[a-z0-9_-]{6,18}$/i;
 
+    // validazioni 
     if (username.value === '') {
         setError(username, 'Il campo username è vuoto');
-    } else if (!userValid.test(username.value)) {
+    } else if (!userValid.test(username.value)) { //per l'espressione regolare
         setError(username, 'Lo Username deve avere almeno 4 caratteri.Inserire solo lettere e numeri')
     } else {
+        // rimuove le classi e pulisce i campi quando l'utente inserisce un nome valido
         username.classList.remove('error-active');
         smallUser.classList.remove('error-active');
         smallUser.innerHTML = ' ';
@@ -60,28 +76,30 @@ function addAccount(e) {
         smallPass2.innerHTML = ' ';
     }
     if (userValid.test(username.value) && emailValid.test(email.value) && passwordValid.test(password.value) && password2.value === password.value) {
+        // se tutti i campi sono corretti pulisci, rimuovi le classi e vai ad un'altra pagina
         username.value = '';
         email.value = '';
         password.value = '';
         password2.value = '';
         removeClass();
-        document.form.submit();
+        document.form.submit(); //nell'html ho inserito nel form l'attributo action con la pagina in cui ci dobbiamo spostare
     }
     e.preventDefault();
 }
 
+///////////////////////////////////////////////////
+/////                 FUNCTION    
+/////////////////////////////////////////////////
 
-// FUNCTION 
 function setError(input, text) {
-    let small = input.nextElementSibling;
+    let small = input.nextElementSibling; //recupero lo small di quell'input
     input.classList.add('error-active');
     small.classList.add('error-active');
-    small.innerHTML = text;
-    console.log(input.nextElementSibling);
-    console.log(text);
+    small.innerHTML = text; //scrivo il testo inserito quando chiamo la funzione
 }
 
 function removeClass() {
+    // rimuove tutte le classi 'error-active' se presenti in input e small e svuoto il campo
     input.forEach(function (e) {
         if (e.classList.contains('error-active')) {
             e.classList.remove('error-active')
@@ -90,7 +108,7 @@ function removeClass() {
     small.forEach(function (e) {
         if (e.classList.contains('error-active')) {
             e.classList.remove('error-active');
-            small.children.innerHTML = '';
+            small.innerHTML = '';
         };
     })
 }
